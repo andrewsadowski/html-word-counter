c9a80779228a97ee3ae6e6fe1dbb3106f82471cb
root <andrew.n.sadowski@gmail.com>
2018-10-25 11:33:35 -0700

    added util.js with helper functions, added urls.txt for testing

---
+++ src/utils.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+
+/**
+ * Creates an array of URLs separated by line breaks
+ * @return {Array} Array of URL strings
+ * TODO: change parseUrls to accept
+ */
+const parseUrls = () => {
+  const urlArr = [];
+  const urls = fs
+    .readFileSync("./scripts/WC_Estimator/urls.txt")
+    .toString()
+    .split("\n");
+  urls.forEach(url => {
+    if (url) urlArr.push(url);
+  });
+  return urlArr;
+};
+
+/**
+ * Appends a new CSV line to the output file
+ * @param  {String} data String to append to file (CSV formatted)
+ */
+const exportToCSV = data => {
+  //create/append data to file
+  fs.appendFile(
+    "./output/wc/URL_WC.csv",
+    data + "\n",
+    err => {
+      if (err) throw err;
+      console.log(data);
+    }
+  );
+};
+
+module.exports = {
+  parseUrls,
+  exportToCSV
+};
